c35e305b480198910eca7e53d0c23ec191a47b1e
AlinaGimp <alinagimp.v@gmail.com>
2022-11-09 15:27:53 +0100

    New dependecies

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": "off",
+    "no-use-before-define": "off",
+    "no-param-reassign": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+  },
+
+
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AirQuality from './air-quality/AirQuality';
+
+export default function App() {
+  return (
+      <AirQuality />
+  );
+}
