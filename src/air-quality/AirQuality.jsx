9d6f67478a11970c716c4969f1a6f5de2085537d
AlinaGimp <alinagimp.v@gmail.com>
2022-11-20 16:00:36 +0100

    Added descriptions of air pollution statuses

--- src/air-quality/AirQuality.jsx
+++ src/air-quality/AirQuality.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AirQualityStyle.css';
 import useGetAirPollutionData from './hook/useGetAirPollutionData';
+import DescriptionStatusAir from './DescriptionStatusAir';
 
 function AirQuality() {
     const forecastAirPollutions = useGetAirPollutionData();
@@ -12,9 +13,8 @@ function AirQuality() {
             <div className="blocks">
                 {forecastAirPollutions.map((forecastAirPollution) => {
                     const displayData = new Date(forecastAirPollution.dt * 1000).toDateString();
-                    const statusAir = forecastAirPollution.main.aqi;
                     const pollution = forecastAirPollution.components;
-
+                    const statusAir = forecastAirPollution.main.aqi;
 
                     return (
 
@@ -22,21 +22,47 @@ function AirQuality() {
                             <p className="weekdayName">
                                 {displayData}
                             </p>
-                            <p className="indexQuality">
-                                {statusAir}
-                            </p>
+                            <div className="indexQuality">
+                                <DescriptionStatusAir
+                                    statusAir={statusAir}
+                                />
+                            </div>
                             <div className="components">
                                 <div className="components_1">
-                                    <p>CO:{pollution.co}</p>
-                                    <p>NO:{pollution.no}</p>
-                                    <p>NO2:{pollution.no2}</p>
-                                    <p>O3:{pollution.o3}</p>
+                                    <p>
+                                        CO:
+                                        {pollution.co}
+                                    </p>
+                                    <p>
+                                        NO:
+                                        {pollution.no}
+                                    </p>
+                                    <p>
+                                        NO2:
+                                        {pollution.no2}
+                                    </p>
+                                    <p>
+                                        O3:
+                                        {pollution.o3}
+                                    </p>
                                 </div>
                                 <div className="components_2">
-                                    <p>SO2:{pollution.so2}</p>
-                                    <p>PM2.5:{pollution.pm2_5}</p>
-                                    <p>PM10:{pollution.pm10}</p>
-                                    <p>NH3:{pollution.nh3}</p>
+                                    <p>
+                                        SO2:
+                                        {pollution.so2}
+                                    </p>
+                                    <p>
+                                        PM2.5:
+                                        {pollution.pm2_5}
+                                    </p>
+                                    <p>
+                                        PM10:
+                                        {pollution.pm10}
+                                    </p>
+                                    <p>
+                                        NH3:
+                                        {pollution.nh3}
+                                    </p>
                                 </div>
                             </div>
                         </div>
