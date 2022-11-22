1d86acf2402f6812ba5cd47f55f7dadcc1d18954
AlinaGimp <alinagimp.v@gmail.com>
2022-11-22 10:57:09 +0100

    Added block with recommendations

--- src/air-quality/AirQuality.jsx
+++ src/air-quality/AirQuality.jsx
@@ -2,78 +2,70 @@ import React from 'react';
 import './AirQualityStyle.css';
 import useGetAirPollutionData from './hook/useGetAirPollutionData';
 import DescriptionStatusAir from './DescriptionStatusAir';
+import co from './IconsAir/co.png';
+import no from './IconsAir/no.png';
+import no2 from './IconsAir/no2.png';
+import o3 from './IconsAir/o3.png';
+import pm2_5 from './IconsAir/pm2_5.png';
+import pm10 from './IconsAir/pm10.png';
+import so2 from './IconsAir/so2.png';
+import nh3 from './IconsAir/nh3.png';
+
 
 function AirQuality() {
-    const forecastAirPollutions = useGetAirPollutionData();
+  const forecastAirPollutions = useGetAirPollutionData();
 
-    return (
-        <div className="airQuality">
-            <h1 className="header">Air pollution forecast for 5 days</h1>
-            <h3 className="city">Ljubljana</h3>
+  return (
+    <div className="airQuality">
+      <h1 className="header">Air pollution forecast for 5 days</h1>
+      <h3 className="city">Ljubljana</h3>
 
-            <div className="blocks">
-                {forecastAirPollutions.map((forecastAirPollution) => {
-                    const displayData = new Date(forecastAirPollution.dt * 1000).toDateString();
-                    const pollution = forecastAirPollution.components;
-                    const statusAir = forecastAirPollution.main.aqi;
+      <div className="blocks">
+        {forecastAirPollutions.map((forecastAirPollution) => {
+          const displayData = new Date(forecastAirPollution.dt * 1000).toDateString();
+          const pollution = forecastAirPollution.components;
+          const statusAir = forecastAirPollution.main.aqi;
 
-                    return (
+          return (
 
-                        <div className="block">
-                            <p className="weekdayName">
-                                {displayData}
-                            </p>
+            <div className="block">
+              <div className="weekdayName">
+                {displayData}</div>
+                  <div className="pollutions">Pollutions, μg/m3:</div>
+              <div className="indexQuality">
+                <DescriptionStatusAir
+                  statusAir={statusAir}
+                />
+                <div className="components">
+                  <div className="components_1">
+                    <div><img className="iconPollution" src={co} alt="carbon monoxide" />
+                    {pollution.co}</div>
+                    <div><img className="iconPollution" src={no} alt="nitrogen monoxide" />
+                        {pollution.no}</div>
+                    <div><img className="iconPollution" src={no2} alt="nitrogen dioxide" />
+                        {pollution.no2}</div>
+                    <div><img className="iconPollution" src={o3} alt="ozone" />
+                        {pollution.o3}</div>
+                  </div>
+                  <div className="components_2">
+                    <div><img className="iconPollution" src={so2} alt="sulphur dioxide" />
+                        {pollution.so2}</div>
+                    <div><img className="iconPollution" src={pm2_5} alt="fine particles matter" />
+                        {pollution.pm2_5}</div>
+                    <div><img className="iconPollution" src={pm10} alt="coarse particulate matter" />
+                        {pollution.pm10}</div>
+                    <div><img className="iconPollution" src={nh3} alt="ammonia" />
+                        {pollution.nh3}</div>
+                  </div>
+                </div>
+              </div>
 
-                            <div className="indexQuality">
-                                <DescriptionStatusAir
-                                    statusAir={statusAir}
-                                />
-                                <div className="components">
-                                    <div className="components_1">
-                                        <p>
-                                            CO:
-                                            {pollution.co}
-                                        </p>
-                                        <p>
-                                            NO:
-                                            {pollution.no}
-                                        </p>
-                                        <p>
-                                            NO2:
-                                            {pollution.no2}
-                                        </p>
-                                        <p>
-                                            O3:
-                                            {pollution.o3}
-                                        </p>
-                                    </div>
-                                    <div className="components_2">
-                                        <p>
-                                            SO2:
-                                            {pollution.so2}
-                                        </p>
-                                        <p>
-                                            PM2.5:
-                                            {pollution.pm2_5}
-                                        </p>
-                                        <p>
-                                            PM10:
-                                            {pollution.pm10}
-                                        </p>
-                                        <p>
-                                            NH3:
-                                            {pollution.nh3}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    );
-                })}
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default AirQuality;
--- src/air-quality/DescriptionStatusAir.js
+++ src/air-quality/DescriptionStatusAir.js
@@ -1,35 +1,109 @@
 import React from 'react';
 import './AirQualityStyle.css';
+import good from './IconsAir/good.png';
+import poor from './IconsAir/poor.png';
+import trees from './IconsAir/trees (1).png';
+import open_windows from './IconsAir/open_windows.png';
+import mask from './IconsAir/mask.png';
+import window from './IconsAir/window.png';
+import air_filter from './IconsAir/air_filter.png';
 
 
 function DescriptionStatusAir({statusAir}) {
     if (statusAir === 1) {
-        return <> <p style={{color: '#3BD614'}}>Good</p>
-        </>
+        return (
+            <>
+                {' '}
+                <img className="iconStatus" src={good}/>
+                <p className="statusName" style={{color: '#3BD614'}}>Good</p>
+                <div>
+                    <div>
+                        <div><img className="iconRecommendation" src={trees}/></div>
+                        <div className="recommendation">Walk outdoors</div>
+                    </div>
+                    <div>
+                    <div><img className="iconRecommendation" src={open_windows}/></div>
+                    <div className="recommendation">Ventilate the house</div>
+                    </div>
+                </div>
+            </>
+        );
     }
 
     if (statusAir === 2) {
-        return <> <p style={{color: '#4DAB2C'}}>Fair</p>
-         </>
+        return (
+            <>
+                {' '}
+                <img className="iconStatus" src={good}/>
+                <p className="statusName" style={{color: '#4DAB2C'}}>Fair</p>
+                <div>
+                    <div>
+                    <div><img className="iconRecommendation" src={trees}/></div>
+                    <div className="recommendation">Walk outdoors</div>
+                    </div>
+                    <div>
+                    <div><img className="iconRecommendation" src={open_windows}/></div>
+                    <div className="recommendation">Ventilate the house</div>
+                    </div>
+                </div>
+            </>
+        );
     }
 
     if (statusAir === 3) {
-        return <> <p style={{color: '#7EBD18'}}>Moderate</p>
-        </>
+        return (
+            <>
+                {' '}
+                <img className="iconStatus" src={good}/>
+                <p className="statusName" style={{color: '#7EBD18'}}>Moderate</p>
+                <div>
+                    <div><img className="iconRecommendation" src={trees}/></div>
+                    <div className="recommendation">Walk outdoors</div>
+                    <div><img className="iconRecommendation" src={open_windows}/></div>
+                    <div className="recommendation">Ventilate the house</div>
+                </div>
+            </>
+        );
     }
 
     if (statusAir === 4) {
-        return <> <p style={{color: '#BD9918'}}>Poor</p>
-         </>
+        return (
+            <>
+                {' '}
+                <img className="iconStatus" src={poor}/>
+                <p className="statusName" style={{color: '#BD9918'}}>Poor</p>
+                <div>
+                    <div><img className="iconRecommendation" src={mask}/></div>
+                    <div className="recommendation">Wear a mask</div>
+                    <div><img className="iconRecommendation" src={window}/></div>
+                    <div className="recommendation">Close windows</div>
+                    <div><img className="iconRecommendation" src={air_filter}/></div>
+                    <div className="recommendation">Use an air purifier</div>
+                </div>
+            </>
+        );
     }
 
     if (statusAir === 5) {
-        return <> <p style={{color: '#BD3518'}}>Very Poor</p>
-     </>
+        return (
+            <>
+        {' '}
+            <img className="iconStatus" src={poor}/>
+            <p className="statusName" style={{color: '#BD3518'}}>Very Poor</p>
+            <div>
+                <div><img className="iconRecommendation" src={mask}/></div>
+                <div className="recommendation">Wear a mask</div>
+                <div><img className="iconRecommendation" src={window}/></div>
+                <div className="recommendation">Close windows</div>
+                <div><img className="iconRecommendation" src={air_filter}/></div>
+                <div className="recommendation">Use an air purifier</div>
+            </div>
+            </>
+        );
     }
 
     return (
-                {statusAir}
+        {statusAir}
     );
 }
 
